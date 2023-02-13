bbb5bb701f826fe8072bdb865ef3269d5be18691
Cassandra Cawley <cassandrabook09@gmail.com>
2023-02-13 12:40:06 -0800

    got powershell working

---
+++ augh.ps1
@@ -0,0 +1,3 @@
+#Invoke-WebRequest https://www.dropbox.com/s/gmyxklhvldkj6aj/followersteleporttoo-1.19.3-2.0.jar?dl=0 -OutFile help.jar
+Invoke-WebRequest https://www.dropbox.com/scl/fo/p7yxf1njrci7mt7x8wu31/h?dl=0&rlkey=kcfb8bi0gcirtad2xp73enmnl -OutFile help.zip
+Read-Host -Prompt "Press Enter to exit"
